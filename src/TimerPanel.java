35b66915b0f4a2db4815598e6b6b1ed7220c5ea1
vin96x <vin96x@gmail.com>
2014-04-05 18:11:57 -0400

    We in there
    Lop.

--- src/TimerPanel.java
+++ src/TimerPanel.java
@@ -2,13 +2,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -16,12 +13,17 @@ import javax.swing.Timer;
 public class TimerPanel extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 3875360370099517808L;
 	private Timer timer;
+	private Timer fading;
+	private Color fade;
 	private int beat = 0;
 	
 	public TimerPanel(){
 		super();
+		fade = new Color(0x323232);
 		timer = new Timer(500, this);
+		fading = new Timer(10,this);
 		timer.start();
+		fading.start();
 		setBackground(new Color(0x92c9ff));
 	}
 	
@@ -32,7 +34,7 @@ public class TimerPanel extends JPanel implements ActionListener {
     	g2d.setFont(new Font("Trebuchet MS", Font.BOLD, 144));
     	for(int i = 0; i < 4; i++) {
 			if(i == beat)
-				g2d.setColor(new Color(0x323232));
+				g2d.setColor(fade);
 			else
 				g2d.setColor(new Color(0xaaaaaa));
 			g2d.drawString(String.valueOf(i + 1), (i+1) * getWidth() / 5 - g2d.getFontMetrics().stringWidth(String.valueOf(i + 1))/2, getHeight()/2);
@@ -45,8 +47,25 @@ public class TimerPanel extends JPanel implements ActionListener {
     }//end paint
     
 	public void actionPerformed(ActionEvent e) {
-		beat = (beat + 1) % 4;
-		timer.setDelay((int) ((1.0/(Game.gamedata.get_bpm()/60.0))*1000.0));
+		Timer t = (Timer)e.getSource();
+		if(t == timer) {
+			beat = (beat + 1) % 4;
+			timer.setDelay((int) ((1.0/(Game.gamedata.get_bpm()/60.0))*1000.0));
+			fade = new Color(0x323232);
+			fading.setDelay((int) (1000.0/Game.gamedata.get_bpm()));
+		}
+		else if(t == fading) {
+			int red = fade.getRed() + 2;
+			int blue = fade.getBlue() + 2;
+			int green = fade.getGreen() + 2;
+			if(red > 170)
+				red = 170;
+			if(blue > 170)
+				blue = 170;
+			if(green > 170)
+				green = 170;
+			fade = new Color(red, green, blue);
+		}
 		repaint();
 	}
 }
