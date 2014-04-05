8e4cdac0d34052c92a6365571ecc7969a1f5e94c
anweshan <anweshanaskar@gmail.com>
2014-04-05 03:44:24 -0400

    made the piano thing prettier and adjustable

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,27 @@
+
+
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Game extends JFrame {
 	
 	private Settings settings;
 	private Timing timing;
+	private PianoPanel p;
 	public Game() {
 		super();
+		setSize(600,700);
+		JPanel panel = new JPanel(new BorderLayout());
 		settings = new Settings();
 		timing = new Timing();
-		add(settings);
-		setLocationRelativeTo(null);
+		p=new PianoPanel();
+		panel.add(p,BorderLayout.CENTER);
+		panel.add(settings,BorderLayout.PAGE_END);
+		
+	getContentPane().add(panel);
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }
--- src/PianoPanel.java
+++ src/PianoPanel.java
@@ -4,35 +4,46 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class PianoPanel extends JPanel {
+	final int width=600; //size of panel
+	final int keylength=50; //sizeofkeys
+	final int offset=20; //offset for formatting
+	int widthWhite=width/45;
+	int widthBlack=widthWhite*2/3;
+	int space=widthWhite*7;
 	public void paint(Graphics g) {
 		super.paint(g);
+	
 		for (int i = 0; i < 45; i++) {
-			drawWhite(i*10, 0, g);
+			drawWhite(i*widthWhite, offset, g);
 		}
 
 		//groups of 3
-		 for (int i=17;i<420;i+=70){
-		 drawBlack(i,0,g);
-		 drawBlack(i+10,0,g);
-		 drawBlack(i+20,0,g);
+		 for (int i=(2*widthWhite-widthBlack/2);i<(width-3*widthWhite);i+=space){
+		 drawBlack(i,offset,g);
+		 drawBlack(i+widthWhite,offset,g);
+		 drawBlack(i+2*widthWhite,offset,g);
 		 }
 		 
 		 //groups of 2
-		 for (int i=57;i<450;i+=70){
-			 drawBlack(i,0,g);
-			 drawBlack(i+10,0,g);
+		 for (int i=(6*widthWhite-widthBlack/2);i<width;i+=space){
+			 drawBlack(i,offset,g);
+			 drawBlack(i+widthWhite,offset,g);
 		 }
-		 drawBlack(437,0,g);
+		 
+		 drawBlack(width-widthWhite*2-widthBlack/2,offset,g);
 
 	}
 
 	public void drawWhite(int x, int y, Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(x, y, widthWhite, keylength);
 		g.setColor(Color.BLACK);
-		g.drawRect(x, y, 10, 50);
+		g.drawRect(x, y, widthWhite, keylength);
 	}
 
 	public void drawBlack(int x, int y, Graphics g) {
 		g.setColor(Color.BLACK);
-		g.fillRect(x, y, 6, 30);
+		g.fillRect(x, y, widthBlack, keylength*3/5);
+
 	}
 }
