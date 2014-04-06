b25250fd3d91f84bd148fb8acd52368511420a4e
vin96x <vin96x@gmail.com>
2014-04-05 20:30:43 -0400

    attempt 2
    Lop.

--- src/ChordPanel.java
+++ src/ChordPanel.java
@@ -33,17 +33,21 @@ public class ChordPanel extends JPanel {
 		g2d.drawString(chrod, getWidth()/2 - g2d.getFontMetrics().stringWidth(chrod)/2, 2*getHeight()/5);
 		g2d.setFont(thefuckingfont);
 		g2d.drawString(next, getWidth()/2 - g2d.getFontMetrics().stringWidth(next)/2, 3*getHeight()/5);
-		if(chords.size() > 0){
+		if(chords.size() > 6) {
 			for(int i = 0; i < 6; i++) {
 				seq += chords.get(i) + " ";
 			}
 			g2d.drawString(seq, getWidth()/2 - g2d.getFontMetrics().stringWidth(seq)/2, 3*getHeight()/4);
-			if(chords.size() > 6) {
-				for(int i = 0; i < chords.size() - 6; i++) {
-					seq += chords.get(i) + " ";
-				}
-				g2d.drawString(seq, getWidth()/2 - g2d.getFontMetrics().stringWidth(seq)/2, 7*getHeight()/8);
+			for(int i = 6; i < chords.size(); i++) {
+				seq += chords.get(i) + " ";
 			}
+			g2d.drawString(seq, getWidth()/2 - g2d.getFontMetrics().stringWidth(seq)/2, 7*getHeight()/8);
+		}
+		else if(chords.size() > 0) {
+			for(int i = 0; i < chords.size(); i++) {
+				seq += chords.get(i) + " ";
+			}
+			g2d.drawString(seq, getWidth()/2 - g2d.getFontMetrics().stringWidth(seq)/2, 3*getHeight()/4);
 		}
 	}
 	
