4c1ee5b4fbf698f3f68ba96e5abe0c7f643e1ddc
vin96x <vin96x@gmail.com>
2014-04-05 21:52:33 -0400

    Color change
    Lop.

--- src/TitlePanel.java
+++ src/TitlePanel.java
@@ -13,8 +13,7 @@ public class TitlePanel extends JPanel {
 	
 	public TitlePanel() {
 		super();
-		setBackground(new Color(0x003864));
-
+		setBackground(new Color(0x111111));
 	}
 	
 	public void paint(Graphics g) {
@@ -22,7 +21,7 @@ public class TitlePanel extends JPanel {
 		Graphics2D g2d = (Graphics2D)g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2d.drawLine(0, getHeight(), getWidth(), getHeight());
-		g2d.setFont(new Font("Trebuchet MS", Font.BOLD, 72));
+		g2d.setFont(new Font("Trebuchet MS", Font.BOLD, 60));
 		g2d.setColor(Color.white);
 		g2d.drawString(title, getWidth()/2 - g2d.getFontMetrics().stringWidth(title)/2, getHeight()/2 + g2d.getFontMetrics().getMaxAscent()/2 - 5);
 	}
